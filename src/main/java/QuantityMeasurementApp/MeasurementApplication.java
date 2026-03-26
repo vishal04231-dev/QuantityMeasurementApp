8b70c7b7ef43e0ca7b951fd8292be15e5703c558
vishal kumar <vishal04231@gmail.com>
2026-03-26 14:28:21 +0530

    Update MeasurementApplication.java

--- src/main/java/QuantityMeasurementApp/MeasurementApplication.java
+++ src/main/java/QuantityMeasurementApp/MeasurementApplication.java
@@ -6,20 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MeasurementApplication {
 
-<<<<<<< HEAD
-    static class Feet{
-        private final double value;
-        public Feet(double value){
-            this.value = value;
-        }
-        public boolean equals(Object obj){
-            if(this == obj) return  true;
-            if(obj == null || getClass() != obj.getClass()){
-                return  false;
-            }
-            Feet other = (Feet)obj;
-            return Double . compare(this.value, other.value) ==0;
-        }
+
     }
 
     public static void main(String[] args) {
@@ -27,10 +14,6 @@ public class MeasurementApplication {
 
         SpringApplication.run(MeasurementApplication.class, args);
 
-        Feet f1 = new Feet(34.5);
-        Feet f2 = new Feet(34.5);
-        System.out.print("Answer: ");
-        System.out.println(f1.equals(f2));
     }
 
 =======
