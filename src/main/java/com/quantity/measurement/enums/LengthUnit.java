0c6dbf0fddb76d4017a4f03ef5858e30a854c8f4
vishal kumar <vishal04231@gmail.com>
2026-04-01 15:31:32 +0530

    Format LengthUnit enum for consistency

--- src/main/java/com/quantity/measurement/enums/LengthUnit.java
+++ src/main/java/com/quantity/measurement/enums/LengthUnit.java
@@ -2,10 +2,10 @@ package com.quantity.measurement.enums;
 
 public enum LengthUnit {
 
-    INCHES(1.0),           // base unit
-    FEET(12.0),            // 1 foot = 12 inches
-    YARDS(36.0),           // 1 yard = 36 inches
-    CENTIMETERS(0.393701); // 1 cm = 0.393701 inches
+    INCHES(1.0),           
+    FEET(12.0),            
+    YARDS(36.0),           
+    CENTIMETERS(0.393701); 
 
     private final double toFeetFactor;
 
